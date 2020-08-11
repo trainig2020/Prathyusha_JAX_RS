86482fd1fd7141574b6a852599b49a67ac9fd54d
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-12 00:10:56 +0530

    Add files via upload

---
+++ REST_messenger_Param_Annotations/src/main/java/org/demo/messenger/service/MessageService.java
@@ -0,0 +1,72 @@
+package org.demo.messenger.service;
+
+import java.util.*;
+
+import org.demo.messenger.database.DatabaseClass;
+import org.demo.messenger.model.Message;
+
+public class MessageService {
+	
+	private Map<Long, Message> messages= DatabaseClass.getMessages();
+	
+	public MessageService() {
+		messages.put(1L,new Message(1,"Hello World","Prathyuhsa"));
+		messages.put(2L,new Message(2,"Hello Jersey","Anusha"));
+		
+	}
+	
+	public List<Message> getAllMessages(){
+		
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	public List<Message> getAllMessagesForYear(int year){
+		List<Message> messagesForyear = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		
+		for(Message message : messages.values()) {
+		   cal.setTime(message.getCreated());
+		   if(cal.get(Calendar.YEAR)== year) {
+			   messagesForyear.add(message);  
+		   }
+	}
+		 return messagesForyear;
+	}
+	
+	public List<Message> getAllMessagesPaginated(int start,int size){
+	     ArrayList<Message> list= new ArrayList<Message>(messages.values());
+	     if(start + size > list.size()) return new ArrayList<Message>();
+	    		
+		 return list.subList(start, start + size);
+	}
+	
+	
+	public  Message getMessage(long id) {
+		return messages.get(id);
+	}
+	
+	
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+		
+	}
+	
+	public Message updateMessage(Message message) {
+		if(message.getId() <= 0) {
+			return null;
+		}
+		
+		messages.put(message.getId(), message);
+		return message;
+		
+	}
+	
+	public Message removeMessage(long id) {
+		return messages.remove(id);
+	}
+	
+	
+	
+}
